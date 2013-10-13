71fc4adbe063180ba2e6900be477c44c00dfeaa6
Sven Günther <sven.guenther@it-agile.de>
2013-10-13 16:28:46 +0200

    Initial Commit

---
+++ Qwirkle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Qwirkle
+//
+//  Created by Sven Günther on 13.10.13.
+//  Copyright (c) 2013 Sven Günther. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
